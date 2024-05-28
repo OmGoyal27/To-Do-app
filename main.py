c7047612fd201597f8ed02109232f23e803123c8
OmGoyal27 <omgoyal2711@gmail.com>
2024-05-28 20:13:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from pathlib import Path
 import json
 
-def extract_tasks():
+def extract_tasks():                    # Extracts all the tasks
     path = Path("todo.json")
     contents = path.read_text()
     return json.loads(contents)
 
-def update(value):
+def update(value):                      # Updates all the tasks
     path = Path("todo.json")
     path.write_text(json.dumps(value))
 
-def extract_commands():
+def extract_commands():                 # Extracts all the available commands
     commands_path = Path("commands.txt")
     commands = commands_path.read_text(encoding='utf-8')
     return commands
 
-def ask():
+def ask():                              # Main thing
     mes = input("\nEnter your command: ")
     ans = mes.lower()
     tasks = extract_tasks()
 
-    if ans == "view":
+    if ans == "view":                   # Shows all the pending tasks
         print("\nThe tasks are:")
         for task in tasks:
             indax = tasks.index(task)
             inndex = indax + 1
             print(f"{inndex}:\t{task}")
-    if ans == "remove":
+    if ans == "remove":                 # Asks to remove a task
         print("\nThe tasks are:")
         for task in tasks:
             indax = tasks.index(task)
@@ -40,13 +40,13 @@ def ask():
         print("Done!")
         update(tasks)
 
-    if ans == "add":
+    if ans == "add":                    # Asks to add a task
         add_task = input("Enter the task you want to add: ")
         tasks.append(add_task)
         update(tasks)
         print(f"The task '{add_task}' has been added to the list.")
 
-    if ans == "help":
+    if ans == "help":                   # Prints all the commands
         print(extract_commands())
 
     if not ans == "exit":                               # Checking to exit
